08bca15d9b94e8b68c8f8360512fa1a596a1ae6d
melinda <melindad_82@msn.com>
2019-12-11 15:42:24 -0500

    Completed DetailView Autolayout, Completed DVC Outlets, Completed tranferring data and configuring background

--- GameOfThrones/GameOfThrones/DetailViewController.swift
+++ GameOfThrones/GameOfThrones/DetailViewController.swift
@@ -11,5 +11,31 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var detailGotRef: GOTEpisode?
-
+    
+    @IBOutlet weak var gotImageView: UIImageView!
+    @IBOutlet weak var gotNameLabel: UILabel!
+    @IBOutlet weak var gotSeasonEpisodeLabel: UILabel!
+    @IBOutlet weak var runtimeLabel: UILabel!
+    @IBOutlet weak var airDateLabel: UILabel!
+    @IBOutlet weak var aboutDescriptionLabel: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        uploadData()
+        
+    }
+   
+    func uploadData() {
+        gotImageView.image = UIImage(named: detailGotRef!.mediumImageID)
+        gotNameLabel.text = detailGotRef!.name.description
+        gotSeasonEpisodeLabel.text = "Season: \(detailGotRef!.season)   Episode: \(detailGotRef!.number)"
+        runtimeLabel.text = "Runtime:  \(detailGotRef!.runtime)"
+        airDateLabel.text = "AirDate:  \(detailGotRef!.airdate)"
+        aboutDescriptionLabel.text = detailGotRef?.summary.description
+        gotNameLabel.numberOfLines = 0
+        gotSeasonEpisodeLabel.numberOfLines = 0
+        runtimeLabel.numberOfLines = 0
+        airDateLabel.numberOfLines = 0
+        
+    }
 }
